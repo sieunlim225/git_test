8ccd3455e8bd836d13d309fd295eed5c2c2d8275
sieunlim <sieunlim@gmail.com>
2021-04-26 11:16:48 +0900

    b2 스쿼시 to master

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -3,13 +3,6 @@ package gitTest;
 public class Test {
 	public static void main(String[] args) {
 		String a = "hello";
-		//before cherry pick@@@@@@@@@@@@@@@@@@@@@@@
-		//this is branch1 work
-		//utf8
-		//this????
-		//eeeeeee
-		//22222222
-		//3333333
-		//wtfffff
+		//utf89999
 	}
 }
--- src/gitTest/Test2.java
+++ src/gitTest/Test2.java
@@ -1,17 +1,5 @@
 package gitTest;
 
 public class Test2 {
-
-    //utf
-
-	//this is branch1 work
-
-	//this is branch1 work2
-
-	//this is branch1 work3
-
-	//this is branch1 work4
-
-
-	//utf8888
+	//utf9999999999999999999999
 }
