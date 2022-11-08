50dc35c5f446ed81334d506b1f55d94273e152a1
sieunlim <sieunlim@jness.co.kr>
2022-11-08 13:55:35 +0900

    Revert "새로시작"
    This reverts commit 787774d1e1dc0c4f545ac432bf266fe6d0e6d312.

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -2,18 +2,8 @@ package gitTest;
 
 public class Test {
 	public static void main(String[] args) {
-
-		//utf899991040
-		class Aa{
-			private int ab;
-			public int getAb(){
-				return this.ab;
-			}
-		}
-
-		Aa ab = new Aa();
-
 		String a = "hello";
+		//utf899991040
 		//�귣ġ2 ����
 		
 		//this is branch1 work
